148795a69a9297d4a3d7809466c3f621fb564014
malu vicentini <maria@multi360.com.br>
2026-09-11 20:31:58 -0300

    chore(spyOn): como mockar o retorno de funcoes

--- src/core/todo/factories/make-validated-todo.spec.ts
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -1,84 +1,43 @@
 import { makeValidatedTodo } from './make-validated-todo';
-import { validateTodoDescription } from '../../todo/schemas/validate-todo-description';
-import { makeNewTodo } from './make-new-todo';
+import * as validateTodoDescriptionMod from '../schemas/validate-todo-description';
 import * as sanitizeStrMod from '../../utils/sanitize-str';
-import * as validateTodoDescriptionMod from '../../todo/schemas/validate-todo-description';
-import * as makeNewTodoMod from '../../todo/factories/make-new-todo';
 
 describe('makeValidatedTodo (unit)', () => {
   test('deve chamar a funcao sanitizeStr com description', () => {
-    const description = 'abcd';
-    const sanitizeStrSpy = vi
-      .spyOn(sanitizeStrMod, 'sanitizeStr')
-      .mockReturnValue(description);
-
+    const { description, sanitizeStrSpy } = makeMocks();
     makeValidatedTodo(description);
-
     expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description);
-    expect(sanitizeStrSpy).toHaveBeenCalledTimes(1);
-    expect(sanitizeStrSpy).toHaveBeenCalledWith(description);
   });
 
-  test('deve chamar a funcao validateTodoDescription com description sanitizada', () => {
-    const description = '    abcd   ';
-    const mockCleanDescription = 'abcd';
+  test('deve chamar validateTodoDescription com o retorno de sanitizeStr', () => {});
+  const { description, sanitizeStrSpy, validaTodoDescriptionSpy } = makeMocks();
 
-    vi.spyOn(sanitizeStrMod, 'sanitizeStr').mockReturnValue(
-      mockCleanDescription,
-    );
-
-    const validateTodoSpy = vi.spyOn(
-      validateTodoDescriptionMod,
-      'validateTodoDescription',
-    );
-
-    makeValidatedTodo(description);
-
-    expect(validateTodoSpy).toHaveBeenCalledTimes(1);
-    expect(validateTodoSpy).toHaveBeenCalledWith(mockCleanDescription);
-  });
-
-  test('deve chamar makeNewTodo se validateDescription retornou success', () => {
-    const description = 'abcd';
-    const mockTodo = {
-      id: '1',
-      description: description,
-      createdAt: '2026-09-11T00:55:37.138Z',
-    };
-
-    vi.spyOn(
-      validateTodoDescriptionMod,
-      'validateTodoDescription',
-    ).mockReturnValue({
-      success: true,
-      errors: [],
-    });
-
-    const makeNewTodoSpy = vi
-      .spyOn(makeNewTodoMod, 'makeNewTodo')
-      .mockReturnValue(mockTodo);
-
-    makeValidatedTodo(description);
-
-    expect(makeNewTodoSpy).toHaveBeenCalledTimes(1);
-    expect(makeNewTodoSpy).toHaveBeenCalledWith(description);
-  });
-
-  test('deve retornar erro se validateDescription falhar', () => {});
-  const description = 'abc';
-  const mockTodoError = ['Description inválida'];
-
-  vi.spyOn(
-    validateTodoDescriptionMod,
-    'validateTodoDescription',
-  ).mockReturnValue({
-    success: false,
-    errors: mockTodoError,
-  });
-
-  const makeNewTodoSpy = vi.spyOn(makeNewTodoMod, 'makeNewTodo');
+  const sanitizeStrReturn = 'Retorno da sanitizeStr';
+  sanitizeStrSpy.mockReturnValue(sanitizeStrReturn);
 
   makeValidatedTodo(description);
 
-  expect(makeNewTodoSpy).not.toHaveBeenCalled();
+  expect(validaTodoDescriptionSpy).toHaveBeenCalledExactlyOnceWith(
+    sanitizeStrReturn,
+  );
 });
+
+// helpers pra esse teste unitario, somente !!!
+const makeMocks = (description = 'abcd') => {
+  const sanitizeStrSpy = vi
+    .spyOn(sanitizeStrMod, 'sanitizeStr')
+    .mockReturnValue(description);
+
+  const validaTodoDescriptionSpy = vi
+    .spyOn(validateTodoDescriptionMod, 'validateTodoDescription')
+    .mockReturnValue({
+      errors: [],
+      success: true,
+    });
+
+  return {
+    description,
+    sanitizeStrSpy,
+    validaTodoDescriptionSpy,
+  };
+};
